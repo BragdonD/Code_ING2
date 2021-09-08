394ff2c86943490515080e4f56b4831424b284c9
BragdonD <73294632+BragdonD@users.noreply.github.com>
2021-09-08 23:25:54 +0200

    exo 200

---
+++ TP2/exo_200/src/main.cpp
@@ -0,0 +1,15 @@
+#include "entete.h"
+
+int main(){
+    int integer;
+    float real;
+    
+    integer = enterInteger();
+    real = enterReal();
+
+    std::cout << "You have typed the following integer : " <<  integer<< std::endl;
+    std::cout << "You have typed the following real : " <<  real << std::endl;
+    
+    return 0;
+}
+
