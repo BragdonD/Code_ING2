e81ec36b49b63084652b55ece3cd840a971b0b31
BragdonD <73294632+BragdonD@users.noreply.github.com>
2021-09-09 16:39:53 +0200

    end of 202

--- TP2/exo_202/src/fonction.cpp
+++ TP2/exo_202/src/fonction.cpp
@@ -56,3 +56,31 @@ std::string concatenateString(std::string string1, std::string string2){
         return string1 + ' ' + string2;
     }
 }
+
+std::string up1ToAsciiOfString(std::string input){
+    std::string output;
+
+    for(int i=0; i<input.size();i++){
+        if(input[i]!='z' || input[i]!='Z'){
+           output.push_back(input[i]+1); 
+        }
+        else if(input[i]=='z'){
+            output.push_back('a'); 
+        }
+        else if(input[i]=='Z'){
+            output.push_back('A'); 
+        }
+    }
+    return output;
+}
+
+void menu(){
+    std::cout << "Make a choice between the following propositions :" << std::endl;
+    std::cout << "1. Type a string." << std::endl;
+    std::cout << "2. Calculate the string size." << std::endl;
+    std::cout << "3. Count te number of vowels and consonant into the string." << std::endl;
+    std::cout << "4. Delete a character from the string." << std::endl;
+    std::cout << "5. Concatenate two string." << std::endl;
+    std::cout << "6. Moove up from 1 ascii code the letters of the string." << std::endl;
+    std::cout << "7 Leave." << std::endl;
+}
