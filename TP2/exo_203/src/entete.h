cb1ba18dfab5abe83ee3741bbb7a5f5e06a845b8
BragdonD <73294632+BragdonD@users.noreply.github.com>
2021-09-09 17:26:46 +0200

    exo 203 tp2

---
+++ TP2/exo_203/src/entete.h
@@ -0,0 +1,11 @@
+#ifndef __ENTETE_H__
+#define __ENTETE_H__
+
+#include <iostream>
+#include <limits>
+
+int enterInteger();
+float enterReal();
+float calculateAverage(float *reals, int size);
+
+#endif
---
+++ TP2/exo_203/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "entete.h"
+
+int main(){
+    float* reals = new float[11];
+    int size(11);
+
+    for(int i=0; i<size-1; i++){
+        reals[i] = enterReal();
+    }
+    reals[size-1] = calculateAverage(reals,size);
+    
+    for(int i=0; i<size; i++){
+        std::cout << reals[i] << std::endl;
+    }
+
+    return 0;
+}
+
+
