06290012b9e8866da26ced199895b27bc0b1a9bd
BragdonD <73294632+BragdonD@users.noreply.github.com>
2021-09-09 19:41:26 +0200

    blindage on n
    blindage sur la valeur de n
    n ne peut plus etre inférieur a 2

--- TP2/exo_206/src/main.cpp
+++ TP2/exo_206/src/main.cpp
@@ -6,7 +6,12 @@ int main(){
     int n;
     int u0(1), u1(1);
     std::cout << "Type an integer n to calculate U(n) : " << std::endl;
-    n = enterInteger();
+    do{
+        n = enterInteger();
+        if(n<2){
+            std::cout << "Impossible to calculate or you already have the answer" << std::endl;
+        }
+    }while(n<2);
     std::cout << "U(n) = " << sequence(u1,u0, n) << std::endl;
     return 0;
 }
