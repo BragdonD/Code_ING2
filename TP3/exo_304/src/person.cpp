4a59689da49b6853ccad7b6cbbfd493aa6de38ce
BragdonD <73294632+BragdonD@users.noreply.github.com>
2021-09-19 21:27:46 +0200

    begin 304

---
+++ TP3/exo_304/src/person.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "person.h"
+
+person::person(/* args */)
+{}
+
+person::~person()
+{}
+
+void person::setFirstName(std::string n_firstName){
+
+}
+
+void person::setLastName(std::string n_lastName){
+
+}
+
+bool person::setSex(std::string n_sex){
+
+}
+
+void person::setBirthday(int n_day, int n_month, int n_year){
+
+}
+
+void person::setPartner(person n_person){
+
+}
+
+std::string person::getFirstName() const{
+
+}
+
+std::string person::getLastName() const{
+
+}
+
+std::string person::getSex() const{
+
+}
+
+person* person::getPartner() const{
+
+}
+
+int person::getYearOfBirth() const{
+
+}
+
+int person::getMonthOfBirth() const{
+
+}
+
+int person::getDayOfBirth() const{
+
+}
+
+void person::Divorce(){
+
+}
+
+void person::Marry(){
+
+}
---
+++ TP3/exo_304/src/person.h
@@ -0,0 +1,36 @@
+#ifndef __PERSON_H__
+#define __PERSON_H__
+#pragma once
+
+#include <string>
+
+class person{
+    private:
+        std::string m_firstName;
+        std::string m_lastName;
+        std::string m_sex;
+        int m_dayOfBirth;
+        int m_monthOfBirth;
+        int m_yearOfBirth;
+        person *m_pPartner;
+
+    public:
+        person(/* args */);
+        ~person();
+        void setFirstName(std::string n_firstName);
+        void setLastName(std::string n_lastName);
+        bool setSex(std::string n_sex);
+        void setBirthday(int n_day, int n_month, int n_year);
+        void setPartner(person n_person);
+        std::string getFirstName() const;
+        std::string getLastName() const;
+        std::string getSex() const;
+        person* getPartner() const;
+        int getYearOfBirth() const;
+        int getMonthOfBirth() const;
+        int getDayOfBirth() const;
+        void Divorce();
+        void Marry();
+};
+
+#endif
