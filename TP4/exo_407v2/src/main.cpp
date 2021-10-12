4f961e84662d73e730210d5e6feac0541aa46acc
BragdonD <73294632+BragdonD@users.noreply.github.com>
2021-10-12 22:10:51 +0200

    amelioration hash table

--- TP4/exo_407v2/src/main.cpp
+++ TP4/exo_407v2/src/main.cpp
@@ -1,26 +1,52 @@
 #include <iostream>
 #include "hashtable.h"
-struct Hash
+
+struct HashPolynom
 {
-    static const std::size_t m = 1e7 + 9;
+    //un nombre premier pour réduire les collisions
+    static const std::size_t m = 1e18 + 9;
     size_t operator()(const std::string& value) const
     {
-        const std::array<std::size_t,26> p{26,676,17576,456976,1881367,8915506,1802949,6876638,8792435,8603112,3680714,5698483,8160432,2171043,6447073,7623754,8217433,3653069,4979713,9472430,6282964,3356920,7279848,9275886,1172820,493293};
-        size_t hash(0LU);
+        size_t hash(0);
+        //variable des puissance de la fonction polynomiale
+        size_t p_pow(1);
+        const int p(42);
         //42 est le nombre de lettre dans l'alphabet fr en comptant les lettres avec des caractères spéciaux
-        int wordSize = value.size();
-        for(int i=0; i<wordSize; i++){
-            hash =  (hash + (value.at(i) - 'a' + 1) * p.at(i)) % m;
+        for(int i=0; i<value.size(); i++){
+            //calcul du terme de la puissance p^i
+            hash = (hash + (value.at(i) - 'a' + 1) * p_pow) % m;
+            //recalcule de la nouvelle puissance
+            p_pow = (p_pow * p) % m;
         }
 
         return hash;
     }
 };
 
+struct HashFirstNb
+{
+    size_t operator()(const std::string& value) const
+    {
+        //un nombre premier
+        size_t hash(503);
+        for(int i=0; i<value.size(); i++){
+            //on multiplie a nouveau par un autre nombre premier
+            hash = (hash+ value.at(i))*541;
+        }
+        return hash;
+    }
+};
+
+
 int main()
 {
-    MyHashTable<Hash> h("words.txt");
-    h.testSpeed(0); // set 1 if you want more challenge
-    h.testSpeed(1); // set 1 if you want more challenge
+    //MyHashTable<HashPolynom> h("words.txt");
+    MyHashTable<HashFirstNb> g("words.txt");
+    //h.testSpeed(0); 
+    //h.testSpeed(1); 
+    //h.testSpeed(2);
+    g.testSpeed(0); 
+    g.testSpeed(1); 
+    g.testSpeed(2); 
     return 0;
 }
