e6fca07fc9ec3632ba3f9b18a71feefbca29f8cf
BragdonD <73294632+BragdonD@users.noreply.github.com>
2021-10-23 18:18:54 +0200

    502

--- TP5/exo_502/src/main.cpp
+++ TP5/exo_502/src/main.cpp
@@ -6,11 +6,24 @@
 #include "dinosaur/velociraptor.h"
 
 int main(){
-    dinosaur *aTriceratop = new triceratop(5,1,15);
+    dinosaur *aTriceratop = new triceratop(3,1,15);
+    dinosaur *anIchtyosaure = new ichtyosaur(15,2,5);
+    dinosaur *aPterodactile = new pterodactil(5,3,2);
+    dinosaur *aVelociraptor = new velociraptor(10,2,0);
+    dinosaur *aPlesiosaure = new plesiosaur(5,1,10);
 
     std::cout << *aTriceratop << std::endl;
+    std::cout << *anIchtyosaure << std::endl;
+    std::cout << *aPlesiosaure << std::endl;
+    std::cout << *aVelociraptor << std::endl;
+    std::cout << *aPterodactile << std::endl;
 
 
     delete aTriceratop;
+    delete anIchtyosaure;
+    delete aPlesiosaure;
+    delete aVelociraptor;
+    delete aPterodactile;
+
     return 0;
 }
