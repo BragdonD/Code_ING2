d6f339e3c60a08a0e54b74c3dd40186abcc42d52
BragdonD <73294632+BragdonD@users.noreply.github.com>
2021-11-01 20:29:45 +0100

    mise en place du plateau

--- Personnal_Project/Chess_Game/include/pion.h
+++ Personnal_Project/Chess_Game/include/pion.h
@@ -1,14 +1,20 @@
 #ifndef PION_H
 #define PION_H
 
-class pion
-{
-private:
-    /* data */
-public:
-    pion(/* args */);
-    ~pion();
-};
+#include <SFML/Graphics/Sprite.hpp>
 
+namespace Chess{
+    class pion
+    {
+        private:
+            enum Value {None = 0, Pawn, Rook, Knight, Bishop, Queen, King, White, Black};
+            std::pair<Value,Value> m_value;
+        public:
+            pion();
+            ~pion();
+            void setValue(Value Color, Value Rank);
+            std::pair<Value,Value> getValue();
+    };
+}
 
 #endif
